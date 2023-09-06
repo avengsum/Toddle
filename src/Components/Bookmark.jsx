b286d72abaca186df54ce9d2f8f7d34577ff9feb
ANWDM <pv4003337@gmail.com>
2023-09-06 18:05:02 +0530

    done

--- src/Components/Bookmark.jsx
+++ src/Components/Bookmark.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import main from '../assets/main.svg'
 import back from '../assets/back.png'
 import { useSelector } from 'react-redux'
+import PostComponent from './PostComponent'
 
 const Bookmark = () => {
   const [search ,setSearch] = useState('')
@@ -14,6 +15,7 @@ const Bookmark = () => {
     setData(selector)
     setFil(selector)
   },[selector])
+  console.log(data)
 
   const handleFilter = (search,bookmark) => {
     const res = bookmark?.filter(x => x.subject.toLowerCase().includes(search.toLowerCase()));
@@ -35,18 +37,28 @@ const Bookmark = () => {
             onChange={(e) =>{
                 setSearch(e.target.value)
 
-                handleFilter(e.target.value,post)
+                handleFilter(e.target.value,data)
             } }
             className="inline-flex w-[400px] border py-1 bg-slate-100 text-xl flex-col gap-2"
             placeholder='🔍 Search'
             />
             <button 
-            onClick={() => handleFilter(search,post)}
+            onClick={() => handleFilter(search,data)}
             class="text-white right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
             </div>
             </div>
           <div>
-
+          <div className='grid grid-cols-4 gap-y-4'>
+        {fil?.length > 0 ? (fil?.map((x) => (
+            <PostComponent key={x.id}
+            subject={x.subject}
+            image={x.image}
+            description={x.description}
+            date={x.Date}
+            id = {x.id}
+             />
+        ))) : <h1 className='text-2xl'>Nothing Here Create some Post by click on create a post</h1>}
+        </div>
           </div>
     </div>
   )
